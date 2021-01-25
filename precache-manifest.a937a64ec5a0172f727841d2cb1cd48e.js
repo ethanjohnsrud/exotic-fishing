3f264d4e996e6c77bb053ede89ebebedce3db295
Ethan Johnsrud <ethanjohnsrud@gmail.com>
2021-01-25 14:13:05 -0600

    Updates

---
+++ precache-manifest.a937a64ec5a0172f727841d2cb1cd48e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f690516d9177deeb03881a1937b668c6",
+    "url": "/exotic-fishing/index.html"
+  },
+  {
+    "revision": "9dd248d6766cf1fbf1e3",
+    "url": "/exotic-fishing/static/css/main.0e9677f4.chunk.css"
+  },
+  {
+    "revision": "c9ef5a954b401a329c9b",
+    "url": "/exotic-fishing/static/js/2.6d3e95be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exotic-fishing/static/js/2.6d3e95be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd248d6766cf1fbf1e3",
+    "url": "/exotic-fishing/static/js/main.78c950c2.chunk.js"
+  },
+  {
+    "revision": "92f89d7e42be344000bf",
+    "url": "/exotic-fishing/static/js/runtime-main.55208cac.js"
+  },
+  {
+    "revision": "4b412776d8227289365cdb36842115d3",
+    "url": "/exotic-fishing/static/media/Travel.4b412776.png"
+  },
+  {
+    "revision": "62749f7f7dd5b09f42905fcbfb179229",
+    "url": "/exotic-fishing/static/media/equiptment.62749f7f.png"
+  },
+  {
+    "revision": "7c578e64b07bf071cf5a6f54ab45f584",
+    "url": "/exotic-fishing/static/media/footer.7c578e64.png"
+  },
+  {
+    "revision": "4f4f7e270d4b2154f911b131bbf6ac34",
+    "url": "/exotic-fishing/static/media/frogFish.4f4f7e27.png"
+  },
+  {
+    "revision": "d4ab33d4d67eee3105f4b40f803abfa7",
+    "url": "/exotic-fishing/static/media/girl.d4ab33d4.png"
+  },
+  {
+    "revision": "aeb616b2b5f8a5a0a6962a582a9825db",
+    "url": "/exotic-fishing/static/media/header.aeb616b2.png"
+  },
+  {
+    "revision": "f534e3aa2d6a7eef6fa7af0d183fd75d",
+    "url": "/exotic-fishing/static/media/lodging.f534e3aa.png"
+  },
+  {
+    "revision": "6cb60ea54c1cc8ecfaa9fb243227de37",
+    "url": "/exotic-fishing/static/media/man.6cb60ea5.png"
+  },
+  {
+    "revision": "56d0a530e02eb80bd517ae8107b7458e",
+    "url": "/exotic-fishing/static/media/octalizard.56d0a530.png"
+  },
+  {
+    "revision": "4956bdc7a4b60bddf66cef5fc88a56d1",
+    "url": "/exotic-fishing/static/media/seaCat.4956bdc7.png"
+  },
+  {
+    "revision": "6e444304b2a94ea69a2e9d431645fe76",
+    "url": "/exotic-fishing/static/media/waders.6e444304.png"
+  }
+]);
